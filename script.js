54c760e88c22449ec7f5fc35b14830d570f79a69
subhadra001 <136347422+subhadra001@users.noreply.github.com>
2025-11-11 16:46:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -42,6 +42,13 @@ document.addEventListener("DOMContentLoaded", () => {
       setTimeout(() => candle.classList.add("lit"), i * 200);
     });
 
+    // 🐰 Show cute GIF entrance
+const cuteGif = document.getElementById("cuteGif");
+setTimeout(() => {
+  cuteGif.classList.add("active");
+}, 800);
+
+
     // Play music
     setTimeout(() => {
       audio.currentTime = 0;
